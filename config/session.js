145d604c35d07c34c72720fcbfede51e8eb4260f
Samuel Olabode <solabode499@gmail.com>
2026-05-15 15:24:56 -0600

    Remove commented-out code in session configuration.

--- config/session.js
+++ config/session.js
@@ -1,34 +1,3 @@
-// const isProduction = process.env.NODE_ENV === 'production';
-
-// const sessionConfig = {
-//     secret: process.env.SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     name: 'voultSessionId',
-
-//     cookie: {
-//         secure: isProduction,
-//         httpOnly: true,
-
-//         sameSite: 'lax',
-
-//         path: '/',
-
-//         maxAge: isProduction
-//             ? 1000 * 60 * 60 * 1
-//             : 1000 * 60 * 60 * 24 * 7
-//     }
-// };
-
-// if (!process.env.SESSION_SECRET && !process.env.SECRET && isProduction) {
-//     throw new Error(
-//         'SESSION_SECRET or SECRET environment variable is required in production'
-//     );
-// }
-
-// module.exports = sessionConfig;
-
-
 const isProduction = process.env.NODE_ENV === 'production';
 
 const sessionConfig = {

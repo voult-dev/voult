0ca064775d326890ac6e74ea309956022b75d3be
Samuel Olabode <solabode223@gmail.com>
2026-07-28 00:58:31 -0600

    refactor(auth): remove CSRF protection middleware from magic link routes to simplify request handling

--- routes/api/magicLink.js
+++ routes/api/magicLink.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../../controllers/api/magicLink');
 
-const { csrfProtection } = require('../../middleware/csrfProtection');
 const { emailBasedLimiter, ipBasedLimiter } = require('../../middleware/advancedRateLimiting');
 
 const catchAsync = require('../../utils/catchAsync');
 
-router.post('/send-magic-link', csrfProtection, emailBasedLimiter, catchAsync(controller.sendLink));
-router.post('/validate-magic-link', csrfProtection, ipBasedLimiter, catchAsync(controller.validateToken));
+router.post('/send-magic-link', emailBasedLimiter, catchAsync(controller.sendLink));
+router.post('/validate-magic-link', ipBasedLimiter, catchAsync(controller.validateToken));
 
 module.exports = router;
